13a945383ef15d7f04b09c5196bb48f96cdf349f
Leonardo Duarte <leonardoduartevargas@gmail.com>
2021-11-10 17:01:55 +0100

    half way

--- zeno_checker.py
+++ zeno_checker.py
@@ -6,13 +6,17 @@ import re
 #OOPing
 
 class Model:
-
     def __init__(self, xmlFile):
         root = ET.parse(xmlFile)
-        self.components = [Component(c) for c in root.findall("template")]
 
+        # GET DECLARATIONS
+
+        self.components = [Component(c, self) for c in root.findall("template")]
+
+        self.channels = []
         self.single_loops = []
         self.synced_loops = []
+
         for component in self.components:
             for cycle in component.get_cycles():
                 syncs = component.get_cycle_synchronisation(cycle)
@@ -30,28 +34,70 @@ class Model:
         for loop in loops:
             pass
 
+class Cycle:
+    def __init__(self, component, transitions):
+        self.component = component
+        self.transitions = transitions
+        self.safe = self.verify()
 
+    def verify(self):
+        clocks = self.component.clocks
+        
+        for c in clocks:
+            reset = False
+            time_req = False
+            for transition in self.transitions:
+                reset = reset or transition.tests_reset(c)
+                time_req = time_req or transition.tests_time_req(c)
+                if reset and time_req:
+                    return True                
+        return False
+
+
+class Channel:
+    def __init__(self, model, name, emmiterTrans, receiverTrans):
+        self.model = model
+        self.name = name
+        self.elements = (emmiterTrans, receiverTrans)
+        self.broadcast = isinstance(receiverTrans, set)
+    
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return self.name
 
 class Component:
-    def __init__(self, compElement):
-        self.declarations = self.load_local_declarations(compElement)
+    def __init__(self, compElement, model):
+        self.model = model
+        self.local_declarations = self.load_local_declarations(compElement)
+        self.declarations = self.local_declarations # GET GLOBAL DECLARATIONS TOO
 
         self.locations = self.load_locations(compElement)
         self.initial = compElement.find("init").attrib["ref"]
         self.transitions = self.load_transitions(compElement)
 
-
         self.graph = self.get_graph()
+
+        clocks = []
+        for dec in self.declarations:
+            if re.match("clock ", dec):
+                clocks = re.split(" |,", dec)[1:]
+                try:
+                    clocks.remove("")
+                except:
+                    None
+        self.clocks = clocks
 
     def load_locations(self, compElement):
         locations = {}
         for l in compElement.findall("location"):
-            loc = Location(l)
+            loc = Location(l, self)
             locations[loc.id] = loc
         return locations
 
     def load_transitions(self, compElement):
-        return [Transition(tran, self.declarations) for tran in compElement.findall("transition")]
+        return [Transition(tran, self) for tran in compElement.findall("transition")]
         
     def load_local_declarations(self, compElement):
         return compElement.find("declaration").text.replace("\n","").split(";")
@@ -65,26 +111,6 @@ class Component:
 
     def get_cycles(self):
         return list(nx.simple_cycles(self.graph))
-
-    def verify_cycle(self, cycle):
-        clocks = []
-        for dec in self.declarations:
-            if re.match("clock ", dec):
-                clocks = re.split(" |,", dec)[1:]
-                try:
-                    clocks.remove("")
-                except:
-                    None
-
-        for c in clocks:
-            reset = False
-            time_req = False
-            for transition in self.get_cycle_transitions(cycle):
-                reset = reset or transition.tests_reset(c)
-                time_req = time_req or transition.tests_time_req(c)
-                if reset and time_req:
-                    return True                
-        return False
 
     def get_cycle_transitions(self, cycle):
         transitions = []
@@ -110,7 +136,7 @@ class Component:
 
 
 class Location:
-    def __init__(self, locElement):
+    def __init__(self, locElement, component):
         self.id = locElement.attrib['id']
         self.name = locElement.find("name").text if locElement.find("name") is not None else "noname" 
 
@@ -121,28 +147,16 @@ class Location:
         return self.name
 
 class Transition:
-    def __init__(self, transitionElement, localDeclarations):
+    def __init__(self, transitionElement, component):
         self.sourceId = transitionElement.find("source").attrib["ref"]
         self.targetId = transitionElement.find("target").attrib["ref"]
         self.labels =  transitionElement.findall("label")
-        self.declarations = localDeclarations
+        self.declarations = component.declarations
 
         self.synchronisation = ""
         for label in self.labels:
             if label.attrib["kind"] == "synchronisation":
                 self.synchronisation = label.text
-
-        #self.reset = any([label.attrib["kind"] == "assignment" and ("x=0" in label.text.replace(" ","")) for label in labels])
-        """
-        for label in labels:
-            if label.attrib["kind"] == "guard":
-                for atom in label.text.replace(" ","").split("&&"):
-                    if ">" in atom:
-                        self.time_req = True
-                        return None
-        self.time_req = False
-        """
-        #self.time_req = any([label.attrib["kind"] == "guard" and ">" in label.text.replace(" ","") for label in labels])
 
     def tests_reset(self,clock):
         return any([label.attrib["kind"] == "assignment" and (f"{clock}=0" in label.text.replace(" ","")) for label in self.labels]) # add comp to :=
