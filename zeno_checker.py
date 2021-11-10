47f845035d22172520f57e7e859144a9daf6dbf9
Leonardo Duarte <leonardoduartevargas@gmail.com>
2021-11-10 15:46:26 +0100

    half syncs

--- zeno_checker.py
+++ zeno_checker.py
@@ -3,6 +3,33 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import re
 
+class Model:
+
+    def __init__(self, xmlFile):
+        root = ET.parse(xmlFile)
+        self.components = [Component(c) for c in root.findall("template")]
+
+        self.single_loops = []
+        self.synced_loops = []
+        for component in self.components:
+            for cycle in component.get_cycles():
+                syncs = component.get_cycle_synchronisation(cycle)
+                if len(syncs) == 0:
+                    self.single_loops.append(cycle)
+                else:
+                    self.synced_loops.append(cycle)
+
+        self.matched_loops = self.get_matched_loops()
+
+    def get_matched_loops(self):
+        loops = self.synced_loops
+
+        matched_loops = []
+        for loop in loops:
+            pass
+
+
+
 class Component:
     def __init__(self, compElement):
         self.declarations = self.load_local_declarations(compElement)
@@ -25,7 +52,7 @@ class Component:
         return [Transition(tran, self.declarations) for tran in compElement.findall("transition")]
         
     def load_local_declarations(self, compElement):
-        return [dec.text.replace("\n","").split(";") for dec in compElement.findall("declaration")][0]
+        return compElement.find("declaration").text.replace("\n","").split(";")
 
     def get_graph(self):
         graph = nx.MultiDiGraph()
@@ -50,23 +77,40 @@ class Component:
         for c in clocks:
             reset = False
             time_req = False
-            for i in range(len(cycle)):
+            for transition in self.get_cycle_transitions(cycle):
+                reset = reset or transition.tests_reset(c)
+                time_req = time_req or transition.tests_time_req(c)
+                if reset and time_req:
+                    return True                
+        return False
+
+    def get_cycle_transitions(self, cycle):
+        transitions = []
+        for i in range(len(cycle)):
                 for transition in self.transitions:
                     if transition.sourceId == cycle[i] and transition.targetId == cycle[(i+1) % len(cycle)]:
-                        reset = reset or transition.tests_reset(c)
-                        time_req = time_req or transition.tests_time_req(c)
-                        if reset and time_req:
-                            return True                
-        return False
+                        transitions.append(transition)
+        return transitions
 
     def get_verified_cycles(self):
         all_cycles = self.get_cycles()
         return [c for c in all_cycles if self.verify_cycle(c)]
 
+    def get_cycle_synchronisation(self, cycle):
+        base_syncs = set([transition.synchronisation for transition in self.get_cycle_transitions(cycle) if transition.synchronisation])
+        syncs = set()
+        for sync in base_syncs:
+            sync = re.sub("\[.*\]","",sync)
+            sync = (sync[0:-1],sync[-1])
+            syncs.add(sync)
+            
+        return syncs
+
+
 class Location:
     def __init__(self, locElement):
         self.id = locElement.attrib['id']
-        self.name = locElement.find("name").text
+        self.name = locElement.find("name").text if locElement.find("name") is not None else "noname" 
 
     def __repr__(self):
         return self.__str__()
@@ -80,6 +124,12 @@ class Transition:
         self.targetId = transitionElement.find("target").attrib["ref"]
         self.labels =  transitionElement.findall("label")
         self.declarations = localDeclarations
+
+        self.synchronisation = ""
+        for label in self.labels:
+            if label.attrib["kind"] == "synchronisation":
+                self.synchronisation = label.text
+
         #self.reset = any([label.attrib["kind"] == "assignment" and ("x=0" in label.text.replace(" ","")) for label in labels])
         """
         for label in labels:
@@ -93,7 +143,7 @@ class Transition:
         #self.time_req = any([label.attrib["kind"] == "guard" and ">" in label.text.replace(" ","") for label in labels])
 
     def tests_reset(self,clock):
-        return any([label.attrib["kind"] == "assignment" and (f"{clock}=0" in label.text.replace(" ","")) for label in self.labels])
+        return any([label.attrib["kind"] == "assignment" and (f"{clock}=0" in label.text.replace(" ","")) for label in self.labels]) # add comp to :=
 
     def tests_time_req(self,clock):
         for label in self.labels:
@@ -104,8 +154,9 @@ class Transition:
                         if any(re.match(cond, atom) for cond in conds):
                             compared = re.split("[><=]", atom)
                             compared.remove(clock)
+                            compared.remove("")
 
-                            if self.evaluate(compared[0]) > 0:
+                            if self.evaluate(compared[0].replace("(","").replace(")","")) > 0:
                                 return True
 
                         conds = [f"{clock}>", f"<{clock}"]
@@ -113,7 +164,7 @@ class Transition:
                             compared = re.split("[><=]", atom)
                             compared.remove(clock)
 
-                            if self.evaluate(compared[0]) >= 0:
+                            if self.evaluate(compared[0]) >= 0: # add an error if evaluation returns None
                                 return True
         return False
         
@@ -122,8 +173,8 @@ class Transition:
             return int(k)
         except:
             for dec in self.declarations:
-                if re.match(f"(const|constant) int {k} *= *", dec):
-                    return int(re.split(f"(const|constant) int {k} *= *", dec)[-1])
+                if re.match(f"(const|constant) int {k} *:?= *", dec):
+                    return int(re.split(f"(const|constant) int {k} *:?= *", dec)[-1])
 
 
     def __repr__(self):
@@ -132,9 +183,12 @@ class Transition:
     def __str__(self):
         return  f'{self.sourceId} --> {self.targetId}'
 
-root = ET.parse("fischer.xml")
 
-components = [Component(c) for c in root.findall("template")]
+
+model = Model("fischer.xml")
+model = Model("train-gate.xml")
+components = model.components
+
 
 print("---")
 print("all locations:")
@@ -158,6 +212,13 @@ print(components[0].verify_cycle(all_cycles[0]))
 print("---")
 print("cycles satisfing sufficient condition:")
 print(components[0].get_verified_cycles())
+
+
+print("---DEBUG SYNCS")
+components[0].get_cycle_synchronisation(components[0].get_cycles()[0])
+
+print("Synced", model.synced_loops)
+print("Single", model.single_loops)
 
 #nx.draw(components[0].get_graph(),with_labels=True, connectionstyle='arc3, rad = 0.1')
 #plt.show()
